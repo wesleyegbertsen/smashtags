5e034045ceada5142ffbf30945ca342999636275
developer <developer@developers-Mac.local>
2016-12-01 00:42:03 +0100

    Initial lecture code

---
+++ Smashtag/Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,57 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by developer on 30/11/16.
+//  Copyright (c) 2016 Wesley Egbertsen. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+    
+    var tweet: Tweet? {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    @IBOutlet weak var tweetProfileImageView: UIImageView!
+    @IBOutlet weak var tweetScreenNameLabel: UILabel!
+    @IBOutlet weak var tweetTextLabel: UILabel!
+    
+    func updateUI() {
+        //reset any existing tweet information
+        tweetTextLabel?.attributedText = nil
+        tweetScreenNameLabel?.text = nil
+        tweetProfileImageView?.image = nil
+        //tweetCreatedLabel?.text = nil
+
+        if let tweet = self.tweet {
+            tweetTextLabel?.text = tweet.text
+            if tweetTextLabel?.text != nil {
+                for _ in tweet.media {
+                    tweetTextLabel.text! += " 📷"
+                }
+            }
+            
+            tweetScreenNameLabel?.text = "\(tweet.user)" //tweet.user.description
+            
+            if let profileImageURL = tweet.user.profileImageURL {
+                if let imageData = NSData(contentsOfURL: profileImageURL) { // blocks main thread!
+                    tweetProfileImageView?.image = UIImage(data: imageData)
+                }
+            }
+            
+//            let formatter = NSDateFormatter()
+//            if NSDate.timeIntervalSinceDate(tweet.created) > 24*60*60 {
+//                formatter.dateStyle = NSDateFormatterStyle.ShortStyle
+//            } else {
+//                formatter.timeStyle = NSDateFormatterStyle.ShortStyle
+//            }
+//            tweetCreatedLabel?.text = formatter.stringFromDate(tweet.created)
+        }
+        
+    }
+    
+}
